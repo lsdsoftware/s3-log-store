de4a8c07323897b353ca1f748725eef01831ea58
Hai Phan <hai.phan@gmail.com>
2025-09-05 11:27:18 +0700

    work-dir test

---
+++ src/work-dir.test.ts
@@ -0,0 +1,65 @@
+import { makeWorkDir } from "./work-dir.js";
+import { describe, expect, objectHaving } from "@service-broker/test-utils"
+import fsp from "fs/promises"
+import path from "path";
+import os from "os"
+
+const workDirPath = path.join(os.tmpdir(), "s3logstore-testworkdir")
+
+describe('work-dir', ({ beforeEach, afterEach, test }) => {
+  let workDir: ReturnType<typeof makeWorkDir>
+
+  beforeEach(async () => {
+    await fsp.mkdir(workDirPath)
+    workDir = makeWorkDir(workDirPath)
+  })
+
+  afterEach(async () => {
+    await fsp.rm(workDirPath, { recursive: true })
+  })
+
+  test('ensureEmpty', async () => {
+    await workDir.ensureEmpty()
+    await fsp.writeFile(path.join(workDirPath, 'dummy'), 'dummy')
+    await workDir.ensureEmpty()
+      .then(
+        () => { throw new Error('!throwAsExpected') },
+        err => expect(err.message, 'Workdir not empty as expected')
+      )
+  })
+
+  test('listChanges', async () => {
+    await fsp.writeFile(path.join(workDirPath, 'before'), 'before')
+    await new Promise(f => setTimeout(f, 100))
+    await fsp.writeFile(path.join(workDirPath, 'checkpoint'), 'checkpoint')
+    await new Promise(f => setTimeout(f, 100))
+    await fsp.writeFile(path.join(workDirPath, 'after'), 'after')
+    expect(await workDir.listChanges(path.join(workDirPath, 'checkpoint')), ['after'])
+  })
+
+  test('deleteInactive', async () => {
+    const now = Date.now()
+    const twentyFourHoursAgo = now - 24*3600_000
+
+    const inactivePath = path.join(workDirPath, 'inactive')
+    await fsp.writeFile(inactivePath, 'inactive')
+    await fsp.utimes(inactivePath, new Date(now), new Date(twentyFourHoursAgo - 10_000))
+
+    const activePath = path.join(workDirPath, 'active')
+    await fsp.writeFile(activePath, 'active')
+    await fsp.utimes(activePath, new Date(now), new Date(twentyFourHoursAgo + 10_000))
+
+    await workDir.deleteInactive(1)
+    expect(await fsp.readdir(workDirPath), ['active'])
+  })
+
+  test('makeWorkFile', async () => {
+    const workFile = workDir.makeWorkFile('first')
+    await workFile.append('line1\nline2\n')
+    expect(await workFile.read(), { payload: 'line1\nline2\n' })
+
+    await workFile.write(11, 'line3\nline4\n')
+    await workFile.append('line5\n')
+    expect(await workFile.read(), { header: { seqNum: 11 }, payload: 'line3\nline4\nline5\n' })
+  })
+})
